467780dc011ef4bcc4d4ebaee1f867464a6a2553
Mohamed Reyad <moreyad06@gmail.com>
2026-02-14 00:00:06 +0200

    Add generic repository and unit-of-work implementations

---
+++ E-Commerce/Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Models;
+
+namespace Domain.Interfaces
+{
+        public interface IUnitOfWork : IDisposable
+        {
+            public IRepository<BaseUser> Users { get; }
+            public IRepository<Order> Orders { get; }
+            public IRepository<OrderItem> OrderItems { get; }
+            public IRepository<Category> Categories { get; }
+            public IRepository<Product> Products { get; }
+        
+            Task<int> SaveAsync();
+        }
+
+}
---
+++ E-Commerce/Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Models;
+using Domain.Interfaces;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private WaffarXEcommerceDBContext _context;
+
+        public UnitOfWork(WaffarXEcommerceDBContext context)
+        {
+            _context = context;
+            Users = new Repository<BaseUser>(_context);
+            Orders = new Repository<Order>(_context);
+            OrderItems = new Repository<OrderItem>(_context);
+            Categories = new Repository<Category>(_context);
+            Products = new Repository<Product>(_context);
+
+        }
+
+        public IRepository<BaseUser> Users { get; }
+        public IRepository<Order> Orders { get; }
+        public IRepository<OrderItem> OrderItems { get; } 
+        public IRepository<Category> Categories { get; } 
+        public IRepository<Product> Products { get; } 
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+    }
+}
