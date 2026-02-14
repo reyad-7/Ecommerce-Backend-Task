a11ef00630689167228dde4212f37146a5eb91c5
Mohamed Reyad <moreyad06@gmail.com>
2026-02-14 22:49:40 +0200

    Add Identity and JWT authentication setup

---
+++ E-Commerce/Application/Extensions/UserExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.DTOS.Auth;
+using Domain.Entities.Models;
+
+namespace Application.Extensions
+{
+    public static class UserExtensions
+    {
+        public static AuthResponseDto 
+            ToAuthResponseDto(this BaseUser user, string token, int DurationInDays)
+        {
+            return new AuthResponseDto
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = token,
+                ExpiresAt = DurationInDays
+            };
+        }
+    }
+}
--- E-Commerce/Infrastructure/DependencyInjection.cs
+++ E-Commerce/Infrastructure/DependencyInjection.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Services.AuthService;
 using Application.Services.CategoryService;
 using Application.Services.ProductService;
+using Application.Services.TokenService;
 using Domain.Entities.Models;
 using Domain.Interfaces.ICategoryService;
 using Domain.Interfaces.IProductService;
+using Domain.Interfaces.ITokenService;
 using Domain.Interfaces.IunitOfWork;
 using Infrastructure.Data;
 using Infrastructure.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 
 namespace Infrastructure
@@ -29,12 +34,51 @@ namespace Infrastructure
                 options.UseSqlServer(
                     configuration.GetConnectionString("DefaultConnection")));
 
+            // Identity
+            services.AddIdentity<BaseUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 6;
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<WaffarXEcommerceDBContext>()
+            .AddDefaultTokenProviders();
+
+            // JWT Authentication
+            var jwtKey = configuration["Jwt:Key"]
+                ?? throw new InvalidOperationException("Jwt:Key is not configured");
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
             // Unit of Work
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             // Services
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<Domain.Interfaces.IAuthService.IAuthServie, AuthService>();
 
             return services;
         }
